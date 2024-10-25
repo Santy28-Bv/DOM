832d5fb27e46066c44a770a86d8ca469b305cf5e
Santiago <167657885+Santy28-Bv@users.noreply.github.com>
2024-10-24 22:15:00 -0600

    noseq commit es

---
+++ Dibujo Hallowen/app.js
@@ -0,0 +1,177 @@
+const lienzo = document.querySelector('#lienzo');
+const ctx = lienzo.getContext('2d');
+
+
+//Esquina superior derecha
+ctx.fillStyle = '#ac8a3e'; 
+ctx.fillRect(700, 0, 100, 100);
+
+//Esquina superior izquierda
+ctx.fillStyle = '#ac8a3e'; 
+ctx.fillRect(0, 0, 100, 100);
+
+// //Fila Superior
+// ctx.fillStyle = '#b69958'; 
+// ctx.fillRect(100, 0, 600, 100);
+
+//Pixel Superior izquierdo
+ctx.fillStyle = '#b69958'; 
+ctx.fillRect(100, 0, 100, 100);
+
+//Pixel
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(200, 0, 100, 100);
+
+//Pixel
+ctx.fillStyle = '#ff0000'; 
+ctx.fillRect(300, 0, 200, 100)
+
+//Pixel
+ctx.fillStyle = '#ff0000'; 
+ctx.fillRect(100, 100, 200, 100)
+
+
+ctx.fillStyle = '#fffe00'; 
+ctx.fillRect(300, 100, 200, 100)
+
+ctx.fillStyle = '#ff0000'; 
+ctx.fillRect(500, 100, 100, 100)
+
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(600, 100, 100, 100)
+//X,Y// Tamaño: Ancho, largo
+
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(100, 200, 100, 100)
+
+ctx.fillStyle = '#ff0000'; 
+ctx.fillRect(200, 200, 100, 100)
+
+ctx.fillStyle = '#fffe00'; 
+ctx.fillRect(300, 200, 200, 100)
+
+ctx.fillStyle = '#ff0000'; 
+ctx.fillRect(500, 200, 100, 100)
+
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(600, 200, 100, 100)
+
+ctx.fillStyle = '#000000'; 
+ctx.fillRect(0, 300, 100, 100)
+
+ctx.fillStyle = '#ff0000'; 
+ctx.fillRect(100, 300, 100, 100)
+
+ctx.fillStyle = '#fffe00'; 
+ctx.fillRect(200, 300, 200, 100)
+
+ctx.fillStyle = '#ff0000'; 
+ctx.fillRect(400, 300, 100, 100)
+
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(500, 300, 200, 100)
+
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(100, 400, 100, 100)
+
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(100, 400, 100, 100)
+
+ctx.fillStyle = '#000000'; 
+ctx.fillRect(200, 400, 400, 100)
+
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(600, 400, 100, 100)
+
+ctx.fillStyle = '#ff0000'; 
+ctx.fillRect(100, 500, 100, 100)
+
+ctx.fillStyle = '#fffe00'; 
+ctx.fillRect(200, 500, 100, 100)
+
+ctx.fillStyle = '#000000'; 
+ctx.fillRect(300, 500, 100, 100)
+
+
+ctx.fillStyle = '#00fbff'; 
+ctx.fillRect(400, 500, 200, 100)
+
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(600, 500, 100, 100)
+
+ctx.fillStyle = '#ff0000'; 
+ctx.fillRect(100, 600, 100, 100)
+
+ctx.fillStyle = '#fffe00'; 
+ctx.fillRect(200, 600, 300, 100)
+
+ctx.fillStyle = '#00fbff'; 
+ctx.fillRect(500, 600, 100, 100)
+
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(600, 600, 100, 100)
+
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(100, 700, 100, 100)
+
+ctx.fillStyle = '#ff0000'; 
+ctx.fillRect(200, 700, 100, 100)
+
+ctx.fillStyle = '#fffe00'; 
+ctx.fillRect(300, 700, 200, 100)
+
+ctx.fillStyle = '#00fbff'; 
+ctx.fillRect(500, 700, 100, 100)
+
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(600, 700, 100, 100)
+
+
+ctx.fillStyle = '#000000'; 
+ctx.fillRect(700, 300, 100, 100)
+
+
+
+//Pixel
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(500, 0, 100, 100);
+
+//Pixel
+ctx.fillStyle = '#aa4747'; 
+ctx.fillRect(500, 0, 100, 100);
+
+
+
+
+//Pixel Superior derecho
+ctx.fillStyle = '#b69958'; 
+ctx.fillRect(600, 0, 100, 100);
+
+// //Fila Inferior
+// ctx.fillStyle = '#b69958'; 
+// ctx.fillRect(100, 700, 600, 100);
+
+//Columna izquierda
+ctx.fillStyle = '#b69958'; 
+ctx.fillRect(0, 100, 100, 200);
+
+//Columna izquierda
+ctx.fillStyle = '#b69958'; 
+ctx.fillRect(0, 400, 100, 400);
+
+//Columna  derecha
+ctx.fillStyle = '#b69958'; 
+ctx.fillRect(700, 100, 100, 200);
+
+//Columna  derecha
+ctx.fillStyle = '#b69958'; 
+ctx.fillRect(700, 400, 100, 400);
+
+
+// //Esquina inferior izquierda
+// ctx.fillStyle = '#ac8a3e'; 
+// ctx.fillRect(0, 700, 100, 100);
+
+// //Esquina inferior derecha
+// ctx.fillStyle = '#ac8a3e'; 
+// ctx.fillRect(700, 700, 100, 100);
